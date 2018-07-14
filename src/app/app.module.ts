b8b5436c4a75b81468045249b4652ee2cd5b0c1e
Jose Antonio Bravo Isidro <ja.bravo.isidro@gmail.com>
2018-07-14 21:14:31 +0100

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MatCardModule, MatInputModule, MatFormFieldModule, MatButtonModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        LoginPageComponent
     ],
     imports: [
+        FormsModule,
         BrowserAnimationsModule,
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        MatCardModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatButtonModule
     ],
     providers: [],
     bootstrap: [AppComponent]
