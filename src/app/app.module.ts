1fd88a8998a247303c07815d2476c0765037a758
Sergio Martínez Mira <sergiomartinezmira@gmail.com>
2018-07-15 19:19:09 +0200

    Material refactor and user profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginPageComponent } from './login-page/login-page.component';
-import { MatCardModule, MatInputModule, MatFormFieldModule, MatButtonModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { NewAccountComponent } from './new-account/new-account.component';
+import { MaterialModule } from './shared/modules/material.module';
 
 @NgModule({
     declarations: [
@@ -20,10 +20,7 @@ import { NewAccountComponent } from './new-account/new-account.component';
         BrowserAnimationsModule,
         BrowserModule,
         AppRoutingModule,
-        MatCardModule,
-        MatInputModule,
-        MatFormFieldModule,
-        MatButtonModule
+        MaterialModule
     ],
     providers: [],
     bootstrap: [AppComponent]
---
+++ src/app/shared/modules/material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule, MatInputModule, MatFormFieldModule, MatButtonModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
